0119e8bf099c2e8a9715c4570d8fc964685be8fc
smmdrl <srm.reporter1@gmail.com>
2021-04-15 01:17:09 +0600

    ENV Added

--- index.js
+++ index.js
@@ -126,7 +126,7 @@ function callSendAPI(sender_psid, response) {
   }, (err, res, body) => {
     if (!err) {
       console.log('message sent!');
-      console.log(res);
+      console.log(body);
     } else {
       console.error("Unable to send message:" + err);
     }
