aaf3f51339dbd406b8bd4148ef42ea86453c189b
smmdrl <srm.reporter1@gmail.com>
2021-04-15 04:05:40 +0600

    Digital Ride Conversation Start 2

--- index.js
+++ index.js
@@ -30,8 +30,7 @@ app.post('/webhook', (req, res) => {
 
       // Gets the body of the webhook event
       let webhook_event = entry.messaging[0];
-      console.log(webhook_event);
-    
+      
       // Get the sender PSID
       let sender_psid = webhook_event.sender.id;
       console.log('Sender PSID: ' + sender_psid);
