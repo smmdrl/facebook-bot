ff0f763960e629c94461e782121b4ce5d7c7cfd5
smmdrl <srm.reporter1@gmail.com>
2021-04-15 02:27:59 +0600

    Digital Ride Conversation Start

--- index.js
+++ index.js
@@ -85,52 +85,31 @@ app.get('/webhook', (req, res) => {
 function handleMessage(sender_psid, received_message) {
   let response;
   
-  // Checks if the message contains text
-  if (received_message.text) {    
-    // Create the payload for a basic text message, which
-    // will be added to the body of our request to the Send API
-    response = {
-      "text": `You sent the message: "${received_message.text}". Now send me an attachment!`
-    }
-  } else if (received_message.attachments) {
-    // Get the URL of the message attachment
-    let attachment_url = received_message.attachments[0].payload.url;
-    response = {
-      "attachment": {
-        "type": "template",
-        "payload": {
-          "template_type": "generic",
-          "elements": [{
-            "title": "Is this the right picture?",
-            "subtitle": "Tap a button to answer.",
-            "image_url": attachment_url,
-            "buttons": [
-              {
-                "type": "postback",
-                "title": "Yes!",
-                "payload": "yes",
-              },
-              {
-                "type": "postback",
-                "title": "No!",
-                "payload": "no",
-              },
-              {
-                "type": "postback",
-                "title": "No!",
-                "payload": "nao",
-              },
-              {
-                "type": "postback",
-                "title": "No!",
-                "payload": "nbo",
-              }
-            ],
-          }]
-        }
+  response = {
+    "attachment": {
+      "type": "template",
+      "payload": {
+        "template_type": "generic",
+        "elements": [{
+          "title": "ডিজিটাল রাইডে যোগাযোগের জন্য আপনাকে ধন্যবাদ। আরও কথোপকথনের জন্য দয়া করে আপনার পছন্দসই ভাষাটি নির্বাচন করুন।\
+          \
+          Thank you for messaging at Digital Ride. Please select your desired language for futher conversation.",
+          "buttons": [
+            {
+              "type": "postback",
+              "title": "বাংলা",
+              "payload": "bn",
+            },
+            {
+              "type": "postback",
+              "title": "English",
+              "payload": "en",
+            }
+          ],
+        }]
       }
     }
-  } 
+  }
   
   // Send the response message
   callSendAPI(sender_psid, response);    
